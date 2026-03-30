1d289926f906a76b3faadd25333a0bab73d4d045
HoangPhuc <tonyphuc9999@gmail.com>
2026-03-30 22:43:04 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-echo "Fixing Odoo missing modules..."
+echo "Starting Odoo..."
 
 odoo \
   -c /etc/odoo/odoo.conf \
   --http-port=$PORT \
   --http-interface=0.0.0.0 \
-  --proxy-mode \
-  -d misproject_mwvg \
-  -i web \
-  --without-demo=all \
-  --stop-after-init
+  --proxy-mode
