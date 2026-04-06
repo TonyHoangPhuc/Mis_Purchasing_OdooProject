e8287fa799b333639cb4d10733b801cb4ae50f1a
HoangPhuc <156822469+TonyHoangPhuc@users.noreply.github.com>
2026-04-06 20:22:05 +0700

    Module Demo xàm thôi nha cho đừng lỗi

--- custom_addons/Modules_Example/__manifest__.py
+++ custom_addons/Modules_Example/__manifest__.py
@@ -1,1 +1,11 @@
-
+{
+    'name': 'Module Demo xàm xàm đừng có tải ',
+    'version': '1.0',
+    'category': 'Inventory',
+    'summary': 'Quan ly mua hang đồ án MIS',
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
